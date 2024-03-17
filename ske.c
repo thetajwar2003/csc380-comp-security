326dc34e69ff5efbf065841fc2c118bc0c1001bf
thetajwar2003 <rahmantajwar131@gmail.com>
2024-03-17 14:14:07 -0400

    moved files

--- ske.c
+++ ske.c
@@ -2,7 +2,7 @@
 #include "prf.h"
 #include <openssl/sha.h>
 #include <openssl/evp.h>
-#include <openssl/hmac.h>
+#include <openssl/hmac.h>z
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> /* memcpy */
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #ifdef LINUX
-#define MMAP_SEQ MAP_PRIVATE|MAP_POPULATE
+#define MMAP_SEQ MAP_PRIVATE | MAP_POPULATE
 #else
 #define MMAP_SEQ MAP_PRIVATE
 #endif
@@ -31,7 +31,7 @@
 /* need to make sure KDF is orthogonal to other hash functions, like
  * the one used in the KDF, so we use hmac with a key. */
 
-int ske_keyGen(SKE_KEY* K, unsigned char* entropy, size_t entLen)
+int ske_keyGen(SKE_KEY *K, unsigned char *entropy, size_t entLen)
 {
 	/* TODO: write this.  If entropy is given, apply a KDF to it to get
 	 * the keys (something like HMAC-SHA512 with KDF_KEY will work).
@@ -42,23 +42,23 @@ size_t ske_getOutputLen(size_t inputLen)
 {
 	return AES_BLOCK_SIZE + inputLen + HM_LEN;
 }
-size_t ske_encrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
-		SKE_KEY* K, unsigned char* IV)
+size_t ske_encrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
+				   SKE_KEY *K, unsigned char *IV)
 {
 	/* TODO: finish writing this.  Look at ctr_example() in aes-example.c
 	 * for a hint.  Also, be sure to setup a random IV if none was given.
 	 * You can assume outBuf has enough space for the result. */
 	return 0; /* TODO: should return number of bytes written, which
-	             hopefully matches ske_getOutputLen(...). */
+				 hopefully matches ske_getOutputLen(...). */
 }
-size_t ske_encrypt_file(const char* fnout, const char* fnin,
-		SKE_KEY* K, unsigned char* IV, size_t offset_out)
+size_t ske_encrypt_file(const char *fnout, const char *fnin,
+						SKE_KEY *K, unsigned char *IV, size_t offset_out)
 {
 	/* TODO: write this.  Hint: mmap. */
 	return 0;
 }
-size_t ske_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
-		SKE_KEY* K)
+size_t ske_decrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
+				   SKE_KEY *K)
 {
 	/* TODO: write this.  Make sure you check the mac before decypting!
 	 * Oh, and also, return -1 if the ciphertext is found invalid.
@@ -66,8 +66,8 @@ size_t ske_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
 	 * for how to do basic decryption. */
 	return 0;
 }
-size_t ske_decrypt_file(const char* fnout, const char* fnin,
-		SKE_KEY* K, size_t offset_in)
+size_t ske_decrypt_file(const char *fnout, const char *fnin,
+						SKE_KEY *K, size_t offset_in)
 {
 	/* TODO: write this. */
 	return 0;
